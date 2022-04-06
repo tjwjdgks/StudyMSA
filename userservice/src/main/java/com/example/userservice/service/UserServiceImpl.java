8bc298d3bcc5c52ccff650ef058dd97445d529f8
tjwjdgks <tjwjdgks43@gmail.com>
2022-04-06 17:46:41 +0900

    add resillience4j-circuit-breaker-example

--- userservice/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -43,6 +45,7 @@ public class UserServiceImpl implements UserService {
     // for rest template
     // private final RestTemplate restTemplate;
     private final OrderServiceClient orderServiceClient;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -93,8 +96,10 @@ public class UserServiceImpl implements UserService {
             log.error(e.getMessage());
         }
         */
-        // feign error decoder
-        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        // feign error decoder add circuit
+        CircuitBreaker circuitbreaker = circuitBreakerFactory.create("circuitbreaker");
+        List<ResponseOrder> orderList = circuitbreaker.run(() -> orderServiceClient.getOrders(userId),
+                throwable -> new ArrayList<>());
         responseUser.setOrders(orderList);
         return responseUser;
     }
